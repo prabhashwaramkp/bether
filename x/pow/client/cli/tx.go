715671c56892b2340aa56fd9e9e9eb19657afd66
Kavinda Prabhashwara <prabhashwara.mkp@gmail.com>
2025-07-14 18:43:54 +0530

    Change the tx.go with github links

--- x/pow/client/cli/tx.go
+++ x/pow/client/cli/tx.go
@@ -4,7 +4,7 @@ import (
     "github.com/cosmos/cosmos-sdk/client"
     "github.com/cosmos/cosmos-sdk/client/flags"
     "github.com/cosmos/cosmos-sdk/client/tx"
-    "github.com/kavinda/bether/x/pow"
+    "github.com/prabhashwaramkp/bether/x/pow"
     "github.com/spf13/cobra"
 )
 
